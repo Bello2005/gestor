e9b06d21a66dffaa0f3f20649baf1535af860c40
Bello2005 <deinerb2.0.0.5@gmail.com>
2025-10-02 11:13:10 -0500

    Guardar cambios antes de cambiar de rama

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ use App\Http\Controllers\UserController;
 
 // Rutas públicas
 Route::get('/', [LoginController::class, 'showLoginForm'])->name('login.show');
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
